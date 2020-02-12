a77cdaadfd4b03aa363f26398d495cc5a7e74b5d
Philipose <aaronphilipose@gmail.com>
2020-02-12 11:23:22 -0600

    Added Form Formatting to move away from crispy

--- maintenance/forms.py
+++ maintenance/forms.py
@@ -42,24 +42,95 @@ class create_maintenance_window(forms.Form):
             label="Cluster Name", 
             choices=CLUSTER_LIST,
             widget=forms.Select(
-                    attrs={'class': 'btn btn-secondary dropdown-toggle'}
+                    attrs={'class': 'btn btn-secondary dropdown-toggle col-lg-4 col-sm-12 col-md-8'}
             )
     )
-    tenant_name = forms.CharField(label="Tenant Name", max_length=100)
-    tags_list = forms.CharField(label="Tag List (Comma Seperated)", required=False)
-    management_zone_name = forms.CharField(label="Management Zone ID", required=False, max_length=100)
-    window_name = forms.CharField(label="Maintenance Window Name", max_length=100)
-    window_planned = forms.ChoiceField(choices=PLANNED_TYPES)
-    window_description = forms.CharField(label='Description', max_length=200)
-    window_recurrence = forms.ChoiceField(choices=RECURRENCE_TYPES)
-    window_day_of_week = forms.ChoiceField(choices=DAYS_OF_WEEK, required=False)
-    window_day_of_month = forms.ChoiceField(choices=DAYS_OF_MONTH, required=False)
-    window_supression = forms.ChoiceField(choices=SUPPRESSION_TYPES)
-    window_start_time = forms.TimeField(label='Window Start Time', input_formats=["%H:%M"], required=False)
-    window_duration = forms.IntegerField(label='Window Duration (in Minutes)', required=False)
-    window_maintenance_start = forms.DateField(label="Maintenance Start (Format Example: 2019-01-15 23:00)", input_formats= ['%Y-%m-%d %H:%M'])
-    window_maintenance_end = forms.DateField(label="Maintenance End (Format Example: 2019-01-15 23:00)", input_formats= ['%Y-%m-%d %H:%M'])
+    tenant_name = forms.CharField(
+            label="Tenant Name",
+            max_length=100,
+            widget=forms.TextInput(attrs={'class': 'form-control col-lg-4 col-sm-12 col-md-8'})
+    )
+    window_name = forms.CharField(
+            label="Maintenance Window Name",
+            max_length=100,
+            widget=forms.TextInput(attrs={'class': 'form-control col-lg-4 col-sm-12 col-md-8'})
+    )
+    window_planned = forms.ChoiceField(
+            choices=PLANNED_TYPES,
+            widget=forms.Select(
+                    attrs={'class': 'btn btn-secondary dropdown-toggle col-lg-4 col-sm-12 col-md-8'}
+            )
+    )
+    window_description = forms.CharField(
+            label='Description',
+            max_length=200,
+            widget=forms.TextInput(attrs={'class': 'form-control col-lg-4 col-sm-12 col-md-8'})
+    )
+    window_recurrence = forms.ChoiceField(
+            choices=RECURRENCE_TYPES,
+            widget=forms.Select(
+                    attrs={'class': 'btn btn-secondary dropdown-toggle col-lg-4 col-sm-12 col-md-8'}
+            )
+    )
+    window_day_of_week = forms.ChoiceField(
+            choices=DAYS_OF_WEEK,
+            required=False,
+            widget=forms.Select(
+                    attrs={'class': 'btn btn-secondary dropdown-toggle col-lg-4 col-sm-12 col-md-8'}
+            )
+    )
+    window_day_of_month = forms.ChoiceField(
+            choices=DAYS_OF_MONTH,
+            required=False,
+            widget=forms.Select(
+                    attrs={'class': 'form-control btn btn-secondary dropdown-toggle col-lg-4 col-sm-12 col-md-8 col-'}
+            )
+    )
+    window_supression = forms.ChoiceField(
+            choices=SUPPRESSION_TYPES,
+            widget=forms.Select(
+                    attrs={'class': 'btn btn-secondary dropdown-toggle col-lg-4 col-sm-12 col-md-8'}
+            )
+    )
+    window_start_time = forms.TimeField(
+            label='Window Start Time',
+            input_formats=["%H:%M"],
+            required=False,
+            widget=forms.TimeInput(attrs={'class': 'form-control col-lg-4 col-sm-12 col-md-8'})
+    )
+    window_duration = forms.IntegerField(
+            label='Window Duration (in Minutes)',
+            required=False,
+            widget=forms.TextInput(attrs={'class': 'form-control col-lg-4 col-sm-12 col-md-8'})
+    )
+    window_maintenance_start = forms.DateField(
+            label="Maintenance Start (Format Example: 2019-01-15 23:00)",
+            input_formats= ['%Y-%m-%d %H:%M'],
+            widget=forms.DateInput(attrs={'class': 'form-control col-lg-4 col-sm-12 col-md-8'})
+    )
+    window_maintenance_end = forms.DateField(
+            label="Maintenance End (Format Example: 2019-01-15 23:00)",
+            input_formats= ['%Y-%m-%d %H:%M'],
+            widget=forms.TextInput(attrs={'class': 'form-control col-lg-4 col-sm-12 col-md-8'})
+    )
+    window_add_filters = forms.BooleanField(
+            widget=forms.CheckboxInput(attrs={'class': 'form-check'}),
+            required=False,
+    )
+    tags_list = forms.CharField(
+            label="Tag List (Comma Seperated)",
+            required=False,
+            widget=forms.TextInput(attrs={'class': 'form-control col-lg-4 col-sm-12 col-md-8'})
+    )
+    management_zone_name = forms.CharField(
+            label="Management Zone ID",
+            required=False,
+            max_length=100,
+            widget=forms.TextInput(attrs={'class': 'form-control col-lg-4 col-sm-12 col-md-8'})
+    )
     
+
+
     def fields_required(self, fields):
         #    """Used for conditionally marking fields as required."""
         for field in fields:
@@ -69,6 +140,7 @@ class create_maintenance_window(forms.Form):
 
     def clean(self):
         recurrence = self.cleaned_data.get('window_recurrence')
+        filtering = self.cleaned_data.get('window_add_filters')
 
         if recurrence != "ONCE":
             self.fields_required(['window_start_time'])
@@ -86,6 +158,11 @@ class create_maintenance_window(forms.Form):
             self.fields_required(['window_day_of_month'])
         else:
             self.cleaned_data['window_day_of_month'] = None
+
+        if filtering:
+            self.fields_required(['tags_list'])
+        else:
+            self.cleaned_data['tags_list'] = None
 
         return self.cleaned_data
     

9c934e4155663de37e726f34b3dafbb681bc35fc
Aaron Philipose <aaronphilipose@gmail.com>
2020-02-18 14:19:27 -0500

    Adding an allowed host for now for testing

--- dynatrace_admin/settings.py
+++ dynatrace_admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+raqb=*g=x%4fr=$i%7yl+ocy@z4tu1x8x6rhz3p2*@v+jztxg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-test']
 
 
 # Application definition

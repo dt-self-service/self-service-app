10f1945916f127b26461ef5a3d24ffc2adce3b3f
Aaron <aaronphilipose@gmail.com>
2020-06-18 09:00:06 -0500

    Fixing Code Smells

--- permission_handler/tests/test_synczones.py
+++ permission_handler/tests/test_synczones.py
@@ -13,10 +13,10 @@ from maintenance.models import ZonePerms as maintenance_zone_perms
 
 
 class SyncZoneAdd(TestCase):
-  MOCK_SERVER_EX = generate_tenant_url(
+  mock_server_ex = generate_tenant_url(
       user_variables.FULL_SET['Dynatrace_LIVE'], 'tenant1') + '/mockserver/expectation'
 
-  MOCK_JSON = [{
+  mock_json = [{
       "httpRequest": {
           "method": "GET",
           "path": "/api/config/v1/managementZones",
@@ -37,24 +37,24 @@ class SyncZoneAdd(TestCase):
       "id": "SyncZones"
   }]
 
-  NAME_LIST = [
+  name_list = [
       "Dynatrace_LIVE|tenant1|Home",
       "Dynatrace_LIVE|tenant1|Home - Docker",
       "Dynatrace_LIVE|tenant1|easyTravel",
       "Dynatrace_LIVE|tenant1|easyTravel - Test"
   ]
 
-  def check_zones_by_content_type(self, content_type_class, NAME_LIST):
+  def check_zones_by_content_type(self, content_type_class, name_list):
     fail_list = []
-    for name in NAME_LIST:
+    for name in name_list:
       if not Permission.objects.filter(name=name, content_type=ContentType.objects.get_for_model(content_type_class)).exists():
         fail_list.append(f"{ContentType.objects.get_for_model(content_type_class)} | {name}")
 
     return fail_list
 
-  def run_sync(self, MOCK_JSON, NAME_LIST):
+  def run_sync(self, mock_json, name_list):
     with no_ssl_verification():
-      requests.put(self.MOCK_SERVER_EX, json=MOCK_JSON, verify=False)
+      requests.put(self.mock_server_ex, json=mock_json, verify=False)
     call_command('synczones')
 
     fail_list = []
@@ -65,24 +65,24 @@ class SyncZoneAdd(TestCase):
 
     for content_type_class in content_type_list:
       fail_list = fail_list + \
-          self.check_zones_by_content_type(content_type_class, NAME_LIST)
+          self.check_zones_by_content_type(content_type_class, name_list)
 
     return fail_list
 
   def test_mz_sync(self):
-    self.assertEquals(self.run_sync(self.MOCK_JSON,self.NAME_LIST), [])
+    self.assertEquals(self.run_sync(self.mock_json,self.name_list), [])
     call_command('flush', '--no-input')
 
 class SyncZoneDeleted(TestCase):
   def test_mz_removed(self):
     sync_zone_add = SyncZoneAdd()
-    fail_list = sync_zone_add.run_sync(sync_zone_add.MOCK_JSON,sync_zone_add.NAME_LIST)
-    MOCK_JSON = sync_zone_add.MOCK_JSON.copy()
-    NAME_LIST = sync_zone_add.NAME_LIST.copy()
+    fail_list = sync_zone_add.run_sync(sync_zone_add.mock_json,sync_zone_add.name_list)
+    mock_json = sync_zone_add.mock_json.copy()
+    name_list = sync_zone_add.name_list.copy()
     
-    MOCK_JSON[0]["httpResponse"]["body"]["json"]['values'].pop(0) #Removing the "HOME" MZ
-    NAME_LIST[0] = "REMOVED - Dynatrace_LIVE|tenant1|Home"
-    fail_list = fail_list + sync_zone_add.run_sync(MOCK_JSON, NAME_LIST)
+    mock_json[0]["httpResponse"]["body"]["json"]['values'].pop(0) #Removing the "HOME" MZ
+    name_list[0] = "REMOVED - Dynatrace_LIVE|tenant1|Home"
+    fail_list = fail_list + sync_zone_add.run_sync(mock_json, name_list)
 
     self.assertEquals(fail_list, [])
     call_command('flush', '--no-input')

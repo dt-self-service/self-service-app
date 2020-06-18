ab8ee55f5d569b6d49ddeb08e2954e8bfcd6a211
Aaron <aaronphilipose@gmail.com>
2020-06-18 09:02:51 -0500

    SELFSERVE-66 #Fixing Dupe Lien

--- permission_handler/management/commands/synczones.py
+++ permission_handler/management/commands/synczones.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import Permission
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 
 # Zone Perms for each app
